5e322b57850d8f0c176f05db69618983c83749a0
buckeyerichsen <buckey.erichsen@gmail.com>
2019-09-10 10:19:09 -0400

    multiplication table

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String [] args) {
+
+        for( int row = 1; row <=12; row++ ){
+
+            for( int col = 1; col <= 12; col++){
+                System.out.print("  " + row*col + "  " );
+
+            }
+            System.out.println();
+        }
+    }
+}
